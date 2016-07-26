8148deda65aad1933909906d6eeb4345fbffc530
thai nguyen <thai nguyen>
2016-07-26 15:46:30 +0700

    Build home

---
+++ thp/js/thp.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+(function ($, Drupal, window, document) {
+
+  'use strict';
+
+  // To understand behaviors, see https://drupal.org/node/756722#behaviors
+  Drupal.behaviors.my_custom_behavior = {
+    attach: function (context, settings) {
+
+      // Place your code here.
+      
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
