f6e0f23e2b5bf3a6728388771ed4a97f4b2a7642
ngotam909 <ngotam909@gmail.com>
2016-07-27 17:38:20 +0700

    update finsh intro.js and intro.css

--- thp/js/intro.js
+++ thp/js/intro.js
@@ -12,23 +12,20 @@
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
   Drupal.behaviors.intro = {
     attach: function (context, settings) {
-			$('#btn_skip').click(function(){
-
-				console.log('a');
-				document.getElementById('videoskip1').pause();
-				
-				$('#videoskip1').hide();
-				$('#btn_skip').hide();
-				$('#logo').hide();
-				$('.img_logoDr').hide();
-				$('#line_skip').hide();
-				document.getElementById('videoskip2').play();
-				function timeOut(){
-					$(window).attr('location','home');
-					};
-				setTimeout(timeOut,5000);
-			});
+	$('#btn_skip').click(function(){
+		document.getElementById('videoskip1').pause();
+		$('#videoskip1').hide();
+		$('#btn_skip').hide();
+		$('#logo').hide();
+		$('.img_logoDr').hide();
+		$('#line_skip').hide();
+		document.getElementById('videoskip2').play();
+		function timeOut(){
+			$(window).attr('location','home');
+			};
+		setTimeout(timeOut,5000);
+		});
+			}
 		}
-  }
 
 })(jQuery, Drupal, this, this.document);
