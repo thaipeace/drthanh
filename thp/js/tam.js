3b7d460cec6a16125492484b32e7dd5f23bdd669
ngotam909 <ngotam909@gmail.com>
2016-07-29 10:05:28 +0700

    DT2

--- thp/js/tam.js
+++ thp/js/tam.js
@@ -10,11 +10,17 @@
   'use strict';
 
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
-  Drupal.behaviors.my_custom_behavior = {
+  Drupal.behaviors.home = {
     attach: function (context, settings) {
 
       // Place your code here.
-      
+
+      $("#btn_wapper").hover(function(){
+      	$('#img_exper').attr('src','http://localhost/thp/sites/default/files/button_02.svg');
+      },
+      function(){
+      	$('#img_exper').attr('src','http://localhost/thp/sites/default/files/button_01.svg');
+      });
     }
   };
 
