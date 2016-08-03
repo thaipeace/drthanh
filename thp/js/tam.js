e49bf63ec819b2866bd867b224c90514fce7918f
thai nguyen <thai nguyen>
2016-08-03 17:12:15 +0700

    DT3

--- thp/js/tam.js
+++ thp/js/tam.js
@@ -11,119 +11,59 @@
 
   // To understand behaviors, see https://drupal.org/node/756722#behaviors
   Drupal.behaviors.home = {
-    attach: function (context, settings) {
-
-      // Place your code here.
-
-      $("#btn_wapper").hover(function(){
-      	$('#img_exper').attr('src','http://localhost/thp/sites/default/files/button_02.svg');
-      },
-      function(){
-      	$('#img_exper').attr('src','http://localhost/thp/sites/default/files/button_01.svg');
+    attach: function (context, settings) {    
+      $(".ubl_img").hover(function(){
+        if ($(this).parent().parent().css('z-index') === '0') {
+          $('.pnt').css({'opacity': 0});
+          $('.points > div').css({'z-index': 0});
+          
+          $(this).parent().parent().find('.pnt').animate({opacity: 1}, 200);
+          $(this).parent().parent().css({'z-index': 1});
+        }
+      }, function(){
+        
       });
 
-      $(".ubl").hover(function(){
-        $(this).css('z-index',6);
+      $('.cloud_topLeft').animate({left:"-200", opacity:"0.5"}, 3000);
+      $('.cloud_topRight').animate({right:"-600", opacity:"0.5"}, 2000);
+      $('.cloud_bottomLeft').animate({left:"-900", opacity:"0.5"}, 4000);
+      
+      $('.wp_p1').animate({top: screen.height/2.8}, 2100);
+      $('.wp_p2').animate({top: screen.height/2.65}, 1800);
+      $('.wp_p3').animate({top: screen.height/2.8}, 1500);
+      $('.wp_p4').animate({top: screen.height/2.95}, 1300);
+      $('.wp_p5').animate({top: screen.height/2.95}, 1100);
+      $('.wp_p6').animate({top: screen.height/2.8}, 900);
+      $('.wp_p7').animate({top: screen.height/2.95}, 700);
+      $('.wp_p8').animate({top: screen.height/3}, 500);
+      $('.wp_p9').animate({top: screen.height/2.75}, 300);
+      $('.wp_p10').animate({top: screen.height/2.65}, 100);
+      
+      function pathAnimate(path, style, width, height) {
+        var svg = '';
+        svg = "<svg width='" + width + "' height='" + height + "' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' xml:space='preserve'>";
+        svg += "<path d='" + path + "' style='" + style.join(";") + "' />";
+        svg += "</svg>";
 
-      },
-      function(){
-        $(this).css('z-index',0);
-      });
-
-      $(".ubl").hover(function(){
-        $(".pnt").css('z-index',4);
-
-      },
-      function(){
-        $(".pnt").css('z-index',0);
-      });
-
-
-
-      $(".wp_p1").hover(function(){
-        $("#pnt1").css('display','block');
-
-      },
-      function(){
-        $("#pnt1").css('display','none');
-      });
-
-      $(".wp_p2").hover(function(){
-        $("#pnt2").css('display','block');
-      },
-      function(){
-        $("#pnt2").css('display','none');
-      });
-
-      $(".wp_p3").hover(function(){
-        $("#pnt3").css('display','block');
-      },
-      function(){
-        $("#pnt3").css('display','none');
-      });
-
-      $(".wp_p4").hover(function(){
-        $("#pnt4").css('display','block');
-      },
-      function(){
-        $("#pnt4").css('display','none');
-      });
-
-      $(".wp_p5").hover(function(){
-        $("#pnt5").css('display','block');
-      },
-      function(){
-        $("#pnt5").css('display','none');
-      });
-
-      $(".wp_p6").hover(function(){
-        $("#pnt6").css('display','block');
-      },
-      function(){
-        $("#pnt6").css('display','none');
-      });
-
-      $(".wp_p7").hover(function(){
-        $("#pnt7").css('display','block');
-      },
-      function(){
-        $("#pnt7").css('display','none');
-      });
-
-      $(".wp_p8").hover(function(){
-        $("#pnt8").css('display','block');
-      },
-      function(){
-        $("#pnt8").css('display','none');
-      });
-
-      $(".wp_p9").hover(function(){
-        $("#pnt9").css('display','block');
-      },
-      function(){
-        $("#pnt9").css('display','none');
-      });
-
-      $(".wp_p10").hover(function(){
-        $("#pnt10").css('display','block');
-      },
-      function(){
-        $("#pnt10").css('display','none');
-      });
-
-    $('.cloud_topLeft').animate({left:"-200", opacity:"0.5"},10000);
-    $('.cloud_topRight').animate({right:"-600", opacity:"0.5"},10000);
-    $('.cloud_bottomLeft').animate({left:"-900", opacity:"0.5"},10000);
-    $('#ubl1').animate({top:"111%"},10501);
-    $('#ubl2').animate({top:"125%"},10502);
-    $('#ubl3').animate({top:"116%"},10503);
-    $('#ubl4').animate({top:"112%"},10504);
-    $('#ubl5').animate({top:"125%"},10505);
-    $('#ubl6').animate({top:"128%"},10506);
-    $('#ubl7').animate({top:"127%"},10507);
-    $('#ubl8').animate({top:"119%"},10508);
-    $('#ubl9').animate({top:"129%"},10509);
-    $('#ubl10').animate({top:"145%"},10510);
+        return svg;
+      }
+      
+      var style = [
+        "fill:none",
+        "stroke:black",
+        "stroke-width:1",
+        "stroke-linejoin:round",
+        "stroke-linecap:round",
+        "stroke-dasharray:1000",
+        "animation: dash 5s 1 linear forwards",
+        "stroke:#ffffff"
+      ];
+      
+      $('.wp_experience').append(pathAnimate('M289 500 L100 500 L100 304', style, 768, screen.height));
+      $('.wp_experience').append(pathAnimate('M270 540 L232 540 L232 475', style, 768, screen.height));
+      $('.wp_experience').append(pathAnimate('M480 500 L548 500 L548 474', style, 768, screen.height));
+      $('.wp_experience').append(pathAnimate('M650 350 L667 350 L667 295', style, 768, screen.height));
+      $('.wp_experience').append(pathAnimate('M768 330 L667 330 L667 295', style, 768, screen.height));
     }
   };
 
